55a6c4616b6487b1aab4553798866053b7e02c61
agent.craig <agent.craig@417dccff-00e8-3e96-9083-95fa5beca125>
2010-10-04 19:13:15 +0000

    Event stub has been added for our event object A really basic man page has been created

---
+++ drinkduinogame/src/event.cpp
@@ -0,0 +1,13 @@
+/*
+ * Event class
+ */
+
+#include "event.h"
+
+Event::Event() {
+
+}
+
+Event::~Event() {
+
+}
---
+++ drinkduinogame/src/event.h
@@ -0,0 +1,14 @@
+/*
+ * This is for party events
+ */
+
+#ifndef __EVENT_H__
+#define __EVENT_H__
+
+class Event {
+public:
+  Event();
+  ~Event();
+};
+
+#endif /* __EVENT_H__ */
